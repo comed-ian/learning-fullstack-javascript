fa1ca817996c882a34ad858a858fc718b4dbf76a
comed-ian <dupont.ian.a@outlook.com>
2020-11-13 23:13:52 -0600

    end Ch2

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
-/* eslint-disable no-console */
-import config, { nodeEnv, logStars } from './config';
-console.log(config, nodeEnv);
-logStars('Function');
+import config from './config';
+import express from 'express';
+import apiRouter from './api';
+ 
+const server = express();
+ 
+server.set('view engine', 'ejs');
+ 
+server.get('/', (req, res) => {
+  res.render('index', {
+    content: 'Hello Express with <em>EJS<em>'
+  });  //render with EJS
+});
+ 
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+ 
+server.listen(config.port, () => {
+  //print on server terminal
+  console.info('Express listening on port ', config.port);
+});
